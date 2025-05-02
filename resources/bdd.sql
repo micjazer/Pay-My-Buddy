6ff93d9705f686a102d4bc72c305c365b6515f69
Michel Jazeron <michel.jazeron@gmail.com>
2025-05-02 13:36:49 +0200

    Commit 11 / Refonte et organisation des scripts SQL.
    J’ai restructuré les scripts SQL en organisant et en documentant davantage leur contenu pour deux environnements distincts : le développement et le test.
    
    J’ai séparé les sections pour les tables, les données et les triggers, et j’ai ajouté des commentaires détaillés pour faciliter leur exécution et compréhension.
    
    J’ai introduit des nouvelles fonctionnalités comme des triggers : par exemple, un déclencheur crée automatiquement un compte après l'ajout d'un utilisateur, et un autre gère les transactions en vérifiant les soldes avant traitement.
    
    J’ai également modifié les données pour les environnements de test et de développement pour inclure plus de cas d'exemple, tout en corrigeant ou étendant les précédentes structures et entrées.
    
    J’ai souhaité avec cette refonte mettre l'accent sur la clarté, la modularité et la gestion appropriée des données et des transactions.

--- resources/bdd.sql
+++ resources/bdd.sql
@@ -1,11 +1,14 @@
 
-DROP DATABASE IF EXISTS pay_my_buddy;
+-- ? Ce script SQL permet de créer les tables de la base de données pay_my_buddy dans un schéma dev pour l'environnement de développement et dans un schéma test pour l'environnement de test.
 
-CREATE DATABASE pay_my_buddy
-    WITH ENCODING='UTF8'
-    LC_COLLATE='fr_FR.UTF-8'
-    LC_CTYPE='fr_FR.UTF-8'
-    TEMPLATE=template0;
+-- ! Créer d'abord la base de données pay_my_buddy dans pgAdmin, puis exécuter ce script SQL pour créer les tables avec le query tool.
+
+
+-- =======================================================================================================
+-- =======================================================================================================
+-- * TABLES POUR L'ENVIRONNEMENT DE DEVELOPPEMENT
+-- =======================================================================================================
+-- =======================================================================================================
 
 
 CREATE SCHEMA IF NOT EXISTS dev;
@@ -33,6 +36,60 @@ CREATE TABLE IF NOT EXISTS relationships (
     validated_at TIMESTAMP,
     PRIMARY KEY (requester_id, receiver_id)
 );
+
+---------------------------------------------------
+-- Créer la table des comptes
+---------------------------------------------------
+CREATE TABLE IF NOT EXISTS accounts (
+    user_id INTEGER PRIMARY KEY REFERENCES users(id) ON DELETE CASCADE,
+    balance NUMERIC(15, 2) DEFAULT 0.00
+);
+
+---------------------------------------------------
+-- Créer la table des transactions
+---------------------------------------------------
+CREATE TABLE IF NOT EXISTS transactions (
+    id SERIAL PRIMARY KEY,
+    sender_id INTEGER REFERENCES users(id) ON DELETE SET NULL,
+    receiver_id INTEGER REFERENCES users(id) ON DELETE SET NULL,
+    amount NUMERIC(15, 2) NOT NULL CHECK (amount > 0),
+    description VARCHAR(250),
+    processed_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+
+
+-- =======================================================================================================
+-- =======================================================================================================
+-- * TABLES POUR L'ENVIRONNEMENT DE TEST
+-- =======================================================================================================
+-- =======================================================================================================
+
+CREATE SCHEMA IF NOT EXISTS test;
+SET search_path TO test;
+
+---------------------------------------------------
+-- Créer la table des utilisateurs
+---------------------------------------------------
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(250) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP
+);
+
+---------------------------------------------------
+-- Créer la table des connexions entre utilisateurs
+---------------------------------------------------
+CREATE TABLE IF NOT EXISTS relationships (
+    requester_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    receiver_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    validated_at TIMESTAMP,
+    PRIMARY KEY (requester_id, receiver_id)
+    );
 
 ---------------------------------------------------
 -- Créer la table des comptes
--- resources/data.sql
+++ resources/data.sql
@@ -1,33 +1,55 @@
 
+-- ? Ce fichier contient les données d'exemple pour les environnements de développement et de test.
+
+-- ! Exécuter ce script SQL dans pgAdmin après celui des tables et avant celui des triggers.
+
+
+-- =======================================================================================================
+-- =======================================================================================================
+-- * DATA POUR L'ENVIRONNEMENT DE DEVELOPPEMENT
+-- =======================================================================================================
+-- =======================================================================================================
+
+
 SET search_path TO dev;
 
+---------------------------------------------------
 -- Insérer cinq utilisateurs
+---------------------------------------------------
 INSERT INTO users (username, email, password)
 VALUES ('Jean', 'jean@gmail.com', '$2a$10$QWDIWUBgiITPXVTEJZ8goeZGmgVuEM6pyhsia1zZ2BLeyznBbNWly'),
        ('Pierre', 'pierre@gmail.com', '$2a$10$QWDIWUBgiITPXVTEJZ8goeZGmgVuEM6pyhsia1zZ2BLeyznBbNWly'),
        ('Paul', 'paul@gmail.com', '$2a$10$QWDIWUBgiITPXVTEJZ8goeZGmgVuEM6pyhsia1zZ2BLeyznBbNWly'),
        ('Jacques', 'jacques@gmail.com', '$2a$10$QWDIWUBgiITPXVTEJZ8goeZGmgVuEM6pyhsia1zZ2BLeyznBbNWly'),
-       ('Denis', 'denis@gmail.com', '$2a$10$QWDIWUBgiITPXVTEJZ8goeZGmgVuEM6pyhsia1zZ2BLeyznBbNWly');
+       ('Denis', 'denis@gmail.com', '$2a$10$QWDIWUBgiITPXVTEJZ8goeZGmgVuEM6pyhsia1zZ2BLeyznBbNWly'),
+       ('François', 'françois@gmail.com', '$2a$10$QWDIWUBgiITPXVTEJZ8goeZGmgVuEM6pyhsia1zZ2BLeyznBbNWly');
+-- ! Le mdp des utilisateurs est Abc123@!
 
--- Le mdp est Abc123@!
-
+---------------------------------------------------
 -- Insérer des connexions entre utilisateurs
+---------------------------------------------------
 INSERT INTO relationships (requester_id, receiver_id, created_at, validated_at)
 VALUES (1, 2, '2023-01-01 10:00:00', '2023-01-02 10:00:00'),
        (2, 3, '2023-02-01 11:00:00', '2023-02-02 11:00:00'),
        (3, 4, '2023-03-01 12:00:00', '2023-03-02 12:00:00'),
        (4, 5, '2023-04-01 13:00:00', '2023-04-02 13:00:00'),
-       (5, 1, '2023-05-01 14:00:00', '2023-05-02 14:00:00');
+       (5, 1, '2023-05-01 14:00:00', '2023-05-02 14:00:00'),
+       (6, 1, '2023-05-01 14:00:00', NULL);
 
+---------------------------------------------------
 -- Insérer des comptes pour chaque utilisateur
+---------------------------------------------------
 INSERT INTO accounts (user_id, balance)
 VALUES (1, 1100.00),
        (2, 2200.00),
        (3, 1300.00),
        (4, 3400.00),
-       (5, 2500.00);
+       (5, 2500.00),
+       (6, 0.00);
 
+---------------------------------------------------
 -- Insérer des transactions entre utilisateurs
+---------------------------------------------------
 INSERT INTO transactions (sender_id, receiver_id, amount, description, processed_at)
 VALUES (1, 2, 50.00, '', '2023-06-01 15:00:00'),
        (1, 2, 50.00, 'Pour le café renversé sur ton clavier', '2023-06-01 15:00:00'),
@@ -35,16 +57,99 @@ VALUES (1, 2, 50.00, '', '2023-06-01 15:00:00'),
        (2, 1, 70.00, 'Pour alimenter mon addiction au streaming de vidéos de chats', '2023-06-03 15:00:00'),
        (1, 5, 80.00, 'Pour les leçons de danse iroquoise', '2023-06-04 15:00:00'),
        (1, 2, 90.00, 'Pour le gâteau mal préparé', '2023-06-05 15:00:00'),
-       (2, 1, 100.00, 'Pour acheter des accessoires pour mon club secret de collectionneurs de bouchons de bouteilles', '2023-06-06 15:00:00'),
+       (2, 1, 100.00, 'Pour acheter des accessoires pour mon club secret de collectionneurs de bouchons de bouteilles',
+        '2023-06-06 15:00:00'),
        (2, 1, 110.00, 'Pour le prêt de ton vélo à six roues', '2023-06-07 15:00:00'),
        (1, 5, 120.00, 'Aide pour financer ma carrière de critique de pizzas', '2023-06-08 15:00:00'),
        (1, 2, 130.00, 'Pour la thérapie après avoir tenté de résoudre un puzzle de 5000 pièces', '2023-06-09 15:00:00'),
-       (5, 1, 140.00, 'Pour la recherche d''un meilleur endroit pour cacher les bonbons des enfants', '2023-06-10 15:00:00'),
+       (5, 1, 140.00, 'Pour la recherche d''un meilleur endroit pour cacher les bonbons des enfants',
+        '2023-06-10 15:00:00'),
        (5, 1, 150.00, 'Pour le chien que tu as promené', '2023-06-11 15:00:00'),
        (1, 5, 160.00, 'Pour financer mon rêve d’être un influenceur de siestes sur Instagram', '2023-06-12 15:00:00'),
        (1, 2, 170.00, 'Pour le prêt non consentie de ta voiture', '2023-06-13 15:00:00'),
-       (1, 5, 180.00, 'Contribution pour mon abonnement à la bibliothèque de memes sur Internet', '2023-06-14 15:00:00'),
-       (1, 5, 190.00, 'Pour investir dans des chaussons qui promettent de transformer chaque pas en un instant de gloire', '2023-06-15 15:00:00'),
+       (1, 5, 180.00, 'Contribution pour mon abonnement à la bibliothèque de memes sur Internet',
+        '2023-06-14 15:00:00'),
+       (1, 5, 190.00,
+        'Pour investir dans des chaussons qui promettent de transformer chaque pas en un instant de gloire',
+        '2023-06-15 15:00:00'),
+       (2, 3, 75.00, 'Cadeau non mérité', '2023-07-01 16:00:00'),
+       (3, 4, 100.00, 'Remboursement pour la blague de mauvais goût', '2023-08-01 17:00:00'),
+       (4, 5, 25.00, 'Pour le cheval en plastique', '2023-09-01 18:00:00'),
+       (5, 1, 10.00, 'Don pour sauver les pandas en Antarctique', '2023-10-01 19:00:00');
+
+
+
+-- =======================================================================================================
+-- =======================================================================================================
+-- * DATA POUR L'ENVIRONNEMENT DE TEST
+-- =======================================================================================================
+-- =======================================================================================================
+
+
+SET search_path TO test;
+
+---------------------------------------------------
+-- Insérer cinq utilisateurs
+---------------------------------------------------
+INSERT INTO users (username, email, password)
+VALUES ('Jean', 'jean@gmail.com', '$2a$10$QWDIWUBgiITPXVTEJZ8goeZGmgVuEM6pyhsia1zZ2BLeyznBbNWly'),
+       ('Pierre', 'pierre@gmail.com', '$2a$10$QWDIWUBgiITPXVTEJZ8goeZGmgVuEM6pyhsia1zZ2BLeyznBbNWly'),
+       ('Paul', 'paul@gmail.com', '$2a$10$QWDIWUBgiITPXVTEJZ8goeZGmgVuEM6pyhsia1zZ2BLeyznBbNWly'),
+       ('Jacques', 'jacques@gmail.com', '$2a$10$QWDIWUBgiITPXVTEJZ8goeZGmgVuEM6pyhsia1zZ2BLeyznBbNWly'),
+       ('Denis', 'denis@gmail.com', '$2a$10$QWDIWUBgiITPXVTEJZ8goeZGmgVuEM6pyhsia1zZ2BLeyznBbNWly'),
+       ('François', 'françois@gmail.com', '$2a$10$QWDIWUBgiITPXVTEJZ8goeZGmgVuEM6pyhsia1zZ2BLeyznBbNWly');
+-- ! Le mdp des utilisateurs est Abc123@!
+
+
+---------------------------------------------------
+-- Insérer des connexions entre utilisateurs
+---------------------------------------------------
+INSERT INTO relationships (requester_id, receiver_id, created_at, validated_at)
+VALUES (1, 2, '2023-01-01 10:00:00', '2023-01-02 10:00:00'),
+       (2, 3, '2023-02-01 11:00:00', '2023-02-02 11:00:00'),
+       (3, 4, '2023-03-01 12:00:00', '2023-03-02 12:00:00'),
+       (4, 5, '2023-04-01 13:00:00', '2023-04-02 13:00:00'),
+       (5, 1, '2023-05-01 14:00:00', '2023-05-02 14:00:00'),
+       (6, 1, '2023-05-01 14:00:00', NULL);
+
+
+---------------------------------------------------
+-- Insérer des comptes pour chaque utilisateur
+---------------------------------------------------
+INSERT INTO accounts (user_id, balance)
+VALUES (1, 1100.00),
+       (2, 2200.00),
+       (3, 1300.00),
+       (4, 3400.00),
+       (5, 2500.00),
+       (6, 0.00);
+
+
+---------------------------------------------------
+-- Insérer des transactions entre utilisateurs
+---------------------------------------------------
+INSERT INTO transactions (sender_id, receiver_id, amount, description, processed_at)
+VALUES (1, 2, 50.00, '', '2023-06-01 15:00:00'),
+       (1, 2, 50.00, 'Pour le café renversé sur ton clavier', '2023-06-01 15:00:00'),
+       (5, 1, 60.00, 'Pour acheter de la patience pendant les réunions interminables', '2023-06-02 15:00:00'),
+       (2, 1, 70.00, 'Pour alimenter mon addiction au streaming de vidéos de chats', '2023-06-03 15:00:00'),
+       (1, 5, 80.00, 'Pour les leçons de danse iroquoise', '2023-06-04 15:00:00'),
+       (1, 2, 90.00, 'Pour le gâteau mal préparé', '2023-06-05 15:00:00'),
+       (2, 1, 100.00, 'Pour acheter des accessoires pour mon club secret de collectionneurs de bouchons de bouteilles',
+        '2023-06-06 15:00:00'),
+       (2, 1, 110.00, 'Pour le prêt de ton vélo à six roues', '2023-06-07 15:00:00'),
+       (1, 5, 120.00, 'Aide pour financer ma carrière de critique de pizzas', '2023-06-08 15:00:00'),
+       (1, 2, 130.00, 'Pour la thérapie après avoir tenté de résoudre un puzzle de 5000 pièces', '2023-06-09 15:00:00'),
+       (5, 1, 140.00, 'Pour la recherche d''un meilleur endroit pour cacher les bonbons des enfants',
+        '2023-06-10 15:00:00'),
+       (5, 1, 150.00, 'Pour le chien que tu as promené', '2023-06-11 15:00:00'),
+       (1, 5, 160.00, 'Pour financer mon rêve d’être un influenceur de siestes sur Instagram', '2023-06-12 15:00:00'),
+       (1, 2, 170.00, 'Pour le prêt non consentie de ta voiture', '2023-06-13 15:00:00'),
+       (1, 5, 180.00, 'Contribution pour mon abonnement à la bibliothèque de memes sur Internet',
+        '2023-06-14 15:00:00'),
+       (1, 5, 190.00,
+        'Pour investir dans des chaussons qui promettent de transformer chaque pas en un instant de gloire',
+        '2023-06-15 15:00:00'),
        (2, 3, 75.00, 'Cadeau non mérité', '2023-07-01 16:00:00'),
        (3, 4, 100.00, 'Remboursement pour la blague de mauvais goût', '2023-08-01 17:00:00'),
        (4, 5, 25.00, 'Pour le cheval en plastique', '2023-09-01 18:00:00'),
